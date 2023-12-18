c0b62729eb4f4dec23da132ed058d5f6467a1192
MarouaneBouf <marouanebouf3@gmail.com>
2023-12-18 19:56:36 +0100

    Update

--- client/src/pages/auth/sign-in.jsx
+++ client/src/pages/auth/sign-in.jsx
@@ -8,24 +8,23 @@ import { useState } from "react";
 import { Link } from "react-router-dom"; 
 import axios from 'axios';
 import {useNavigate} from "react-router-dom"
-import { useDispatch } from "react-redux";
-import { plus } from "@/features/counter/CounterSlice";
+
 export function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [userNotFound, setUserNotFound] = useState(false);
   const navigate = useNavigate();
-  const dispatch=useDispatch()
+  
   const onClick = () => {
     const body = {
       email: email,
       password: password,
     };
   
-    axios.post("http://localhost:3000/users/checkUser", body)
+    axios.post("http://localhost:8080/user/checkUser", body)
       .then(response => {
         if (response.status === 200) {
-          dispatch(plus(response.data.id))
+          // Ghadi nmchiw men had lpage lpage akhura
           navigate("/dashboard/home")
         } else {
           console.log("lose");
@@ -39,15 +38,15 @@ export function SignIn() {
   };
 
   return (
-    <section className="m-8 flex gap-4 font-switzer">
+    <section className="m-8 flex gap-4 font">
       <div className="w-full lg:w-3/5 mt-24">
         <div className="text-center">
-          <Typography variant="h2" className="font-bold mb-4">Connexion </Typography>
-          <Typography variant="paragraph" color="blue-gray" className="text-lg font-normal">Devenez membre chez Wraaqi.</Typography>
+          <Typography variant="h2" className="font-bold font-CG mb-4">Connexion </Typography>
+          <Typography variant="paragraph" color="blue-gray" className="text-lg font-CG font-normal">Devenez membre chez Wraaqi.</Typography>
         </div>
         <form className="mt-8 mb-2 mx-auto w-80 max-w-screen-lg lg:w-1/2">
           <div className="mb-1 flex flex-col gap-6">
-            <Typography variant="small" color="blue-gray" className="-mb-3 font-medium">
+            <Typography variant="small" color="blue-gray" className="-mb-3 font-CG font-medium">
             Votre email
             </Typography>
             <Input
@@ -59,14 +58,14 @@ export function SignIn() {
               }}
               onChange={(e)=>{setEmail(e.target.value)}}
             />
-            <Typography variant="small" color="blue-gray" className="-mb-3 font-medium">
+            <Typography variant="small" color="blue-gray" className="-mb-3 font-medium font-CG">
               Mot de passe
             </Typography>
             <Input
               type="password"
               size="lg"
               placeholder="********"
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900 font-CG"
               labelProps={{
                 className: "before:content-none after:content-none",
               }}
@@ -77,17 +76,17 @@ export function SignIn() {
           <Button onClick={onClick} className="mt-6" fullWidth>
               connexion
           </Button>
-          {userNotFound?<div className="flex justify-center items-center text-red-500 m-2 text-[14px] font-semibold">User Not Found!</div>:null}
+          {userNotFound?<div className="font-CG flex justify-center items-center text-red-500 m-2 text-[14px] font-semibold">User Not Found!</div>:null}
 
           <div className="flex items-center justify-between gap-2 mt-6">
-            <Typography variant="small" className="font-medium text-gray-900">
+            <Typography variant="small" className="font-medium text-gray-900 font-CG">
               <a href="#">
               Mot de passe oublié?
               </a>
             </Typography>
           </div> 
-          <Typography variant="paragraph" className="text-center text-blue-gray-500 font-medium mt-4">
-          Pas encore inscrit ?<Link to="/auth/sign-up" className="text-gray-900 ml-1">Faites une demande</Link>
+          <Typography variant="paragraph" className="text-center text-blue-gray-500 font-medium mt-4 font-CG">
+          Pas encore inscrit ?<Link to="/auth/sign-up" className="text-gray-900 ml-1 font-CG">Faites une demande</Link>
           </Typography>
         </form>
 
--- client/src/pages/auth/sign-up.jsx
+++ client/src/pages/auth/sign-up.jsx
@@ -46,88 +46,88 @@ export function SignUp() {
       <div className="w-2/5 h-full hidden lg:block">
         <img
           src="/img/Casa.jpg"
-          className="h-[125vh] w-full object-cover rounded-3xl"
+          className="h-[80vh] w-full object-cover rounded-3xl"
         />
       </div>
       <div className="w-full lg:w-3/5 flex flex-col items-center justify-center">
         <div className="text-center">
-          <Typography variant="h2" className="font-bold mb-4">Rejoignez-nous aujourd'hui</Typography>
-          <Typography variant="paragraph" color="blue-gray" className="text-lg font-normal">Entrez votre informations personnelles pour vous inscrire.</Typography>
+          <Typography variant="h2" className="font-bold mb-4 font-CG">Rejoignez-nous aujourd'hui</Typography>
+          <Typography variant="paragraph" color="blue-gray" className="text-lg font-normal font-CG">Entrez votre informations personnelles pour vous inscrire.</Typography>
         </div>
         <form className="mt-5 mb-2 mx-auto w-80 max-w-screen-lg lg:w-1/2">
           <div className="mb-1 flex flex-col gap-6">
-            <Typography variant="small" color="blue-gray" className="-mb-6 font-medium">
+            <Typography variant="small" color="blue-gray" className="-mb-6 font-medium font-CG">
               Prénom
             </Typography>
             <Input
               size="lg"
               placeholder="Saisir votre prénom"
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900 font-CG"
               labelProps={{
                 className: "before:content-none after:content-none",
               }}
               onChange={(event) => { setFirstName(event.target.value) }}
             />
 
-            <Typography variant="small" color="blue-gray" className="-mb-6 font-medium">
+            <Typography variant="small" color="blue-gray" className="-mb-6 font-medium font-CG">
               Nom
             </Typography>
             <Input
               size="lg"
               placeholder="Saisir votre nom"
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900 font-CG"
               labelProps={{
                 className: "before:content-none after:content-none",
               }}
               onChange={(event) => { setLastName(event.target.value) }}
             />
 
-            <Typography variant="small" color="blue-gray" className="-mb-6 font-medium">
+            <Typography variant="small" color="blue-gray" className="-mb-6 font-medium font-CG">
               Adresse e-mail
             </Typography>
             <Input
               size="lg"
               placeholder="nom@mail.com"
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900 font-CG"
               labelProps={{
                 className: "before:content-none after:content-none",
               }}
               onChange={(event) => { setEmail(event.target.value) }}
             />
 
-            <Typography variant="small" color="blue-gray" className="-mb-6 font-medium">
+            <Typography variant="small" color="blue-gray" className="-mb-6 font-medium font-CG">
               Code d'identité national
             </Typography>
             <Input
               size="lg"
               placeholder="Saisir votre CIN"
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900 font-CG"
               labelProps={{
                 className: "before:content-none after:content-none",
               }}
               onChange={(event) => { setCin(event.target.value) }}
             />
 
-            <Typography variant="small" color="blue-gray" className="-mb-6 font-medium">
+            <Typography variant="small" color="blue-gray" className="-mb-6 font-medium font-CG">
               Age
             </Typography>
             <Input
               size="lg"
               placeholder="Saisir votre age"
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900 font-CG"
               labelProps={{
                 className: "before:content-none after:content-none",
               }}
               onChange={(event) => { setAge(event.target.value) }}
             />
 
-            <Typography variant="small" color="blue-gray" className="-mb-6 font-medium">
+            <Typography variant="small" color="blue-gray" className="-mb-6 font-medium font-CG">
               Nationalité
             </Typography>
             <Input
               size="lg"
               placeholder="Saisir votre nationalité"
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900 font-CG"
               labelProps={{
                 className: "before:content-none after:content-none",
               }}
@@ -137,13 +137,13 @@ export function SignUp() {
           <Button className="mt-6" fullWidth onClick={onClick}>
             Inscrivez-vous maintenant
           </Button>
-          {accdemande?<div className="flex justify-center items-center text-green-500 m-2 text-[14px] font-semibold">demande envoye par succ!</div>:null}
+          {accdemande?<div className="flex justify-center items-center text-green-500 m-2 text-[14px] font-CG font-semibold">demande envoye par succ!</div>:null}
 
           <div className="space-y-4 mt-8">
           </div>
-          <Typography variant="paragraph" className="text-center text-blue-gray-500 font-medium mt-4">
+          <Typography variant="paragraph" className="text-center font-CG text-blue-gray-500 font-medium mt-4">
             Vous avez déjà un compte?
-            <Link to="/auth/sign-in" className="text-gray-900 ml-1">Connectez-vous</Link>
+            <Link to="/auth/sign-in" className="text-gray-900 ml-1 font-CG">Connectez-vous</Link>
           </Typography>
         </form>
 
